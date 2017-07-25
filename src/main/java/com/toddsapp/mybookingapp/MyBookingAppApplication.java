441b7a28ec9968e84ddbc0ceb1c61a0201f096be
Todd Anderson <sleepycloudtodd@gmail.com>
2017-07-25 10:19:27 -0500

    "set up project"

---
+++ src/main/java/com/toddsapp/mybookingapp/MyBookingAppApplication.java
@@ -0,0 +1,12 @@
+package com.toddsapp.mybookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookingAppApplication.class, args);
+	}
+}
